b6f21c8610531f8696d219a2d31e866b7ea4973e
zJanny <48256174+zJanny@users.noreply.github.com>
2022-01-23 12:44:48 +0100

    Added simple score system

--- snake_python/libs/game.py
+++ snake_python/libs/game.py
@@ -45,7 +45,11 @@ class Game:
         self.screen.fill(pygame.Color(0, 0, 0))
         self.screen.blit(
             pygame.font.SysFont("monospace", 48).render("Game Over", True, pygame.Color(255, 255, 255)),
-            (self.width / 2 - 100, self.height / 2 - 10)
+            (self.width / 2 - 100, self.height / 2 - 10),
+        )
+        self.screen.blit(
+            pygame.font.SysFont("monospace", 48).render("Score: " + str(self.getPlayerSnake().getScore()), True, pygame.Color(255, 255, 255)),
+            (self.width / 2 - 100, self.height / 2 + 30)
         )
         pygame.display.update()
         pygame.time.wait(2000)
@@ -94,7 +98,8 @@ class Game:
                         quit()
 
             self.update()
-            self.food.checkCollision(self.getPlayerSnake())
+            if self.food.checkCollision(self.getPlayerSnake()):
+                self.getPlayerSnake().addScore()
             
             pygame.display.update()
 
